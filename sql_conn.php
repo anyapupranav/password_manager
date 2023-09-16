2bb0a71f69ffccede86dc5d4b72bec1cd73ba72b
pranavanyapu <anyapupranav@gmail.com>
2023-09-17 02:12:59 +0530

    Update sql_conn.php

--- sql_conn.php
+++ sql_conn.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Asia/Kolkata');
 // Database connection (replace with your credentials)
     $servername = "127.0.0.1";
-    $username = "kishoreweb";
-    $password = "password@123";
+    $username = "root";
+    $password = "";
     $dbname = "password_manager";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
